22b6a8f78176cdb45381d0a5bb266509d8e3be8d
Michael Matveev <xtreme@rh1.ru>
2017-09-04 16:51:41 +0300

    Example module WIP

--- modules/example/frontend.js
+++ modules/example/frontend.js
@@ -23,6 +23,29 @@ module.exports = function(app) {
         // Send to browser
         return res.send(html);
     };
+    // Async function to be used as a filter for pages
+    const filter1Async = async(par1, par2) => {
+        // We will need database here.
+        // But you can simply paste the string below
+        // somewhere outside of your filter1Async function
+        const db = app.get('db');
+        try {
+            // Let's make a dummy query to demonstrate how to use
+            // the async functions here
+            let testData = await db.collection('registry').findOne({ name: 'pagesFolders' });
+            // You can use as many parameters as you wish
+            return 'par1: ' + par1 + ', par2: ' + par2;
+
+        } catch (e) {
+            return 'Error';
+        }
+    };
+    // Callback-style function to be used as Nunjucks filter.
+    // You may simple make a wrap for an async function here.
+    // const navigationDesktop = (data, callback) => {
+    const filter2 = (par1, par2, callback) => {
+        callback(null, '');
+    };
     let router = Router();
     // Let's use example route
     router.get('/', content);
