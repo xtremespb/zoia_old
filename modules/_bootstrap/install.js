39fc0aaaa7335a4542163baed2d473dd56dd7989
Michael A. Matveev <xtreme@rh1.ru>
2017-11-13 17:46:14 +0300

    Bootstrap modules added

---
+++ modules/_bootstrap/install.js
@@ -0,0 +1,24 @@
+/* eslint max-len: 0 */
+const ObjectId = require('mongodb').ObjectID;
+module.exports = function(data) {
+    return async() => {
+        const db = data.db;
+        const config = data.config;
+        console.log('  └── Creating collection: registry...');
+        try {
+            await db.createCollection('registry');
+        } catch (e) {
+            console.log('      [ ] Collection is not created');
+        }
+        console.log('      Dropping indexes...');
+        try {
+            await db.collection('registry').dropIndexes();
+        } catch (e) {
+            console.log('      [ ] Indexes are not dropped');
+        }
+        console.log('      Creating indexes...');
+        await db.collection('registry').createIndex({ name: 1 });
+        await db.collection('registry').createIndex({ name: -1 });
+        console.log('      Module is installed!');
+    };
+};
