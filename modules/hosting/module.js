8073595fd9ec9289b7a1e334c66b74e03979ebde
Michael A. Matveev <xtreme@rh1.ru>
2018-04-26 23:31:43 +0200

    Bugfixes

--- modules/hosting/module.js
+++ modules/hosting/module.js
@@ -3,6 +3,19 @@ module.exports = function(app) {
     const api = require(path.join(__dirname, 'api.js'))(app);
     const backend = require(path.join(__dirname, 'backend.js'))(app);
     const frontend = require(path.join(__dirname, 'frontend.js'))(app);
+    let configModule;
+    try {
+        configModule = require(path.join(__dirname, 'config', 'hosting.json'));
+    } catch (e) {
+        configModule = require(path.join(__dirname, 'config', 'hosting.dist.json'));
+    }
+    if (configModule.paymentPlugin) {
+        try {
+            require(path.join(__dirname, 'plugins_payment', configModule.paymentPlugin + '.js'))(app, frontend.routes);
+        } catch (e) {
+            app.get('log').error('Could not load payments plugin: ' + e);
+        }
+    }
     app.get('log').info('[hosting] module loaded');
     return {
         frontend: {
--- modules/warehouse/module.js
+++ modules/warehouse/module.js
@@ -3,13 +3,20 @@ module.exports = function(app) {
     const api = require(path.join(__dirname, 'api.js'))(app);
     const backend = require(path.join(__dirname, 'backend.js'))(app);
     const frontend = require(path.join(__dirname, 'frontend.js'))(app);
-    app.get('log').info('[warehouse] module loaded');
     let configModule;
     try {
         configModule = require(path.join(__dirname, 'config', 'catalog.json'));
     } catch (e) {
         configModule = require(path.join(__dirname, 'config', 'catalog.dist.json'));
     }
+    if (configModule.payments && configModule.payments.enabled) {
+        try {
+            require(path.join(__dirname, 'plugins', configModule.payments.plugin, 'routes'))(app, frontend.routes);
+        } catch (e) {
+            app.get('log').error('Could not load payments plugin: ' + e);
+        }
+    }
+    app.get('log').info('[warehouse] module loaded');
     return {
         frontend: {
             prefix: configModule.prefix,
