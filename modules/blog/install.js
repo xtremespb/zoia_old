e1c8d4e2dcd5a4527c2ea286aac771005b808547
Michael A. Matveev <xtreme@rh1.ru>
2018-05-21 17:27:27 +0300

    Blog WIP

--- modules/blog/install.js
+++ modules/blog/install.js
@@ -1,1 +1,11 @@
 module.exports = function() {};
+
+/*console.log('  └── Installing required packages (this may take a while)...');
+        try {
+            await npm.install(['csvtojson@1.1.9'], {
+                cwd: path.join(__dirname, '..', '..'),
+                save: false
+            });
+        } catch (e) {
+            console.log('      Error: ' + e);
+        }*/
